90df66c4c5dceb506c378b5dd336d09080edabcf
Delta062004 <22bcs037@iiitdwd.ac.in>
2023-05-25 00:48:35 +0530

    Getting rid of the Hashtag
    I did this one in the Return_0 coding contest and solved it using stacks.

---
+++ Tutorial_presentation/hash.c
@@ -0,0 +1,85 @@
+/* You are given the Instagram post caption of a popular influencer which contains hashtags. 
+   In one operation, you can: 
+   ● Choose a hash in the caption. 
+   ● Remove the closest non-hash character to its left, as well as remove the hash symbol itself. 
+   Print the caption after all hashes have been removed. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 35
+
+int stack[MAX];
+int top = -1;
+
+char pop()
+{
+    if(top != -1)
+    {
+        char c = stack[top];
+        top = top - 1;
+        return c;
+    }
+    else 
+        printf("Stack Underflow\n");
+    exit(101);
+}
+
+void push(char c)
+{
+    if(top != MAX - 1)
+    {
+        top = top + 1;
+        stack[top] = c;
+        return;
+    }
+    else 
+        printf("Stack Overflow\n");
+    return;
+}
+
+void display()
+{
+    if(top != -1)
+    {
+        for(int i = 0; i <= top; i++)
+        {
+            printf("%c", stack[i]);
+        }
+        printf("\n");
+        return;
+    }
+    else
+        printf("Stack Underflow\n");
+    return;
+}
+
+int main()
+{
+    printf("Enter the String\n\n");
+    char arr[MAX];
+    scanf("%s", arr);
+    int len = strlen(arr);
+    char c;
+
+    for(int i = 0; i < len; i++)
+    {
+        if(isalnum(arr[i]))
+        {
+            push(arr[i]);
+        }
+        else if(arr[i] == '#')
+        {
+            if(top == -1)
+                continue;
+            c = pop();
+        }
+        else 
+            continue;
+    }
+    printf("The actual string is\t");
+    display();
+    return 0;
+}
